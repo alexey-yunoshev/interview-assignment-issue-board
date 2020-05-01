9517bb6bbac5262401dc1d69523aafe9c0c67c42
Alexey <a.yunoshev@elama.ru>
2020-05-01 13:38:36 +0300

    update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,5 @@
 docker-compose -f docker-compose.dev.yaml down --remove-orphans -v
 docker-compose -f docker-compose.prod.yaml down --remove-orphans -v
 docker-compose -f docker-compose.test-environment.yaml down --remove-orphans -v
+docker-compose -f docker-compose.test-api.yaml down --remove-orphans -v
+docker-compose -f docker-compose.test-ui.yaml down --remove-orphans -v
